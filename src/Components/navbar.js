3cc3bc3c40e25e93f81e67f60c77e75ba99ba8ac
Usa7rm <115411688+Usa7rm@users.noreply.github.com>
2024-05-11 11:45:01 +0300

    bug fix

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -10,7 +10,7 @@ import { Link as RouterLink } from 'react-router-dom';
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Voting App
             </Typography>
-            <Button color="inherit" component={RouterLink} to="/">Home</Button>
+            <Button color="inherit" component={RouterLink} to="/Home">Home</Button>
             <Button color="inherit" component={RouterLink} to="/results">Results</Button>
             <Button color="inherit" component={RouterLink} to="/login">Login</Button>
             <Button color="inherit" component={RouterLink} to="/winner">Winner</Button>
