fd60221ce813af65767f8d751739d99a1ca7d23e
Usa7rm <115411688+Usa7rm@users.noreply.github.com>
2024-05-11 11:13:34 +0300

    small ui changes nav bar

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import Winner from './Pages/winner';
 import Results from './Pages/Results';
 import Navbar from './Components/navbar';
-
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#302b63', // Indigo shade as the primary color
-    },
-    secondary: {
-      main: '#24243e', // Dark blue for secondary actions
-    },
-    background: {
-      default: '#0f0c29', // Deep violet for the overall background
-    },
-    text: {
-      primary: '#ffffff', // White text for clarity
-    },
-  },
-  components: {
-    MuiAppBar: {
-      styleOverrides: {
-        root: {
-          background: 'linear-gradient(45deg, #0f0c29, #302b63)', // Gradient background
-        }
-      }
-    },
-  },
-});
-
 
 
 function App() {
--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,39 +1,11 @@
 import React from 'react';
-import { AppBar, Toolbar, Button, Typography, useMediaQuery } from '@mui/material';
-import { useTheme, createTheme, ThemeProvider } from '@mui/material/styles';
+import { AppBar, Toolbar, Button, Typography } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#302b63', // Indigo shade as the primary color
-      },
-      secondary: {
-        main: '#24243e', // Dark blue for secondary actions
-      },
-      background: {
-        default: '#0f0c29', // Deep violet for the overall background
-      },
-      text: {
-        primary: '#ffffff', // White text for clarity
-      },
-    },
-    components: {
-      MuiAppBar: {
-        styleOverrides: {
-          root: {
-            background: 'linear-gradient(45deg, #0f0c29, #302b63)', // Gradient background
-          }
-        }
-      },
-    },
-  });
+
   function Navbar() {
-    const theme = useTheme();
-    const matches = useMediaQuery(theme.breakpoints.up('sm'));
-  
     return (
-      <ThemeProvider theme={theme}>
-        <AppBar position="static">
+      <>
+        <AppBar position="static" sx={{backgroundColor:'#0f0c29'}}>
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
               Voting App
@@ -44,7 +16,7 @@ const theme = createTheme({
             <Button color="inherit" component={RouterLink} to="/winner">Winner</Button>
           </Toolbar>
         </AppBar>
-      </ThemeProvider>
+      </>
     );
   }
   
